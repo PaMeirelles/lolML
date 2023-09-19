7ced6eb3ff27193600a8b0fbda8a9fc226ba8caa
PaMeirelles <rafaelpmeirelles@.com>
2023-09-19 15:45:22 -0300

    corrigindo fill with median

--- dataframe_pipeline/na_dealers.py
+++ dataframe_pipeline/na_dealers.py
@@ -9,7 +9,14 @@ def remove_na(df):
     return df.dropna()
 
 def fill_with_median(df):
-    return df.fillna(df.median())
+    for column in df.columns:
+        non_na_values = df[column].dropna()
+        if non_na_values.empty:
+            df.drop(column, axis=1, inplace=True)
+        else:
+            non_na_median = non_na_values.median()
+            df[column].fillna(non_na_median, inplace=True)
+    return df
 
 # Wrappers
 def keep_columns_above_5_percent(df):
